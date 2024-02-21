a9179ada205f897aaa72f4ac40dec3b00a6a706d
Leaked method <129183717+leaked-method@users.noreply.github.com>
2024-02-21 17:45:27 +0600

    Update duck.js

--- duck.js
+++ duck.js
@@ -1,4 +1,4 @@
 var logoText = "Mifta";
 var siteName = "Mifta";
-var bestLink = "https://countlessdispleased.com/bnnr520i1v?key=0280dfd41f9808f55fe46a20c364c652";
+var bestLink = "https://adoptioneitherrelaxing.com/svhsjsiy?key=ac9d2f2f1d2970a68806f5ae702dd5e5";
 document.title = "Click Here to watch";
