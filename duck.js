93d30e551db88f04f997a0c6dc52089fb20107e6
Leaked method <129183717+leaked-method@users.noreply.github.com>
2024-03-10 22:29:06 +0600

    Delete duck.js

--- duck.js
+++
@@ -1,4 +0,0 @@
-var logoText = "Maisha";
-var siteName = "Maisha";
-var bestLink = "https://adoptioneitherrelaxing.com/svhsjsiy?key=ac9d2f2f1d2970a68806f5ae702dd5e5";
-document.title = "Click Here To Watch";
